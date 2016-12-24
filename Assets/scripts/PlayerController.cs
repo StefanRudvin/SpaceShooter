c2acd2f844e93dd237c77a3fb560e3a0ca00d93b
Stefan Rudvin <Stefan.rudvin@hotmail.com>
2016-12-24 02:50:17 +0200

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public Boundary boundary;
+
+	public float sideTilt;
+	public float frontTilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+
+			nextFire = Time.time + fireRate;
+
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+	}
+
+	// Before each fixed physics step
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.velocity =  movement * speed;
+
+		//rb.AddForce (someVector3Value);
+
+		rb.position = new Vector3
+		(
+			Mathf.Clamp(rb.position.x,boundary.xMin,boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z,boundary.zMin,boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (rb.velocity.z * frontTilt, 0.0f, rb.velocity.x * -sideTilt);
+
+
+	}
+}
